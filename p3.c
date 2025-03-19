460bf8b86c1590d8496d44d5e95eea55d42a2312
Francisco Javier Corbera Peña <corbera@ac.uma.es>
2025-03-19 10:11:58 +0100

    Update p3.c

--- p3.c
+++ p3.c
@@ -2,23 +2,22 @@
 #include <stdio.h>
 #include <string.h>
 
-#define LONGITUD 20
+#define LONGITUD 32
 
 int main()
 {
-
     char buffer[LONGITUD];
+    char buffer2[LONGITUD] = "Esto no !!";
     char *p_cadena;
     int i, j;
 
     j = 0;
-    for (i = 0; i < 20; i++)
+    for (i = 0; i < LONGITUD; i++)
     {
-        ++j;
-        if (j > 9)
-            j = 1;            /*Vuelve a 1 tras el 9 */
         buffer[i] = j + 0x30; /* Código ASCII del dígito */
-
+        j++;
+        if (j > 9)
+            j = 0; /* Vuelve a 0 tras el 9 */
         /* OJO: El buffer no contiene todavía una cadena; no está terminada en 0!! */
     }
 
@@ -28,14 +27,14 @@ int main()
     printf("La longitud del buffer es %d \n", LONGITUD);
     printf("La cadena más larga que puede albergar este buffer es, por tanto, de %d caracteres \n\n", LONGITUD - 1);
     printf("Iterando con el puntero, el contenido del buffer es =");
-    for (i = 0; i < 20; i++)
+    for (i = 0; i < LONGITUD; i++)
     {
         /* Imprimimos en pantalla el contenido del buffer */
         printf("%c", *(p_cadena++));
     }
     printf("\n");
     printf("Iterando sobre el array, el contenido del buffer es =");
-    for (i = 0; i < 20; i++)
+    for (i = 0; i < LONGITUD; i++)
     {
         /* Imprimimos en pantalla el contenido del buffer */
         printf("%c", buffer[i]);
